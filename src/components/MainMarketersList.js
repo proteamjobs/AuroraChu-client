fc6212aec8db604533a006d0b82ac21f4dac682c
sehyeonpark <ongsim1@naver.com>
2019-08-09 16:26:22 +0900

    default main page complete

--- src/components/MainMarketersList.js
+++ src/components/MainMarketersList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ReactStars from "react-stars";
 import "./styles.css";
 
 class MainMarketersList extends Component {
@@ -24,19 +25,62 @@ class MainMarketersList extends Component {
   render() {
     if (this.state.marketersList.length !== 0) {
       return (
-        <div className="marketerList">
-          {this.state.marketersList.map(item => {
-            return (
-              <div key={item.marketer_id}>
-                <div>
-                  <img style={{ width: 30, height: 30 }} src={item.image_url} />
+        <div>
+          <p style={{ textAlign: "left" }}>최근 등록된 마케터</p>
+          <div className="marketerList">
+            {this.state.marketersList.map(item => {
+              return (
+                <div className="listItem" key={item.marketer_id}>
+                  <div>
+                    <img
+                      style={{ width: "100%", height: 100 }}
+                      src={item.image_url}
+                    />
+                  </div>
+                  <div
+                    style={{
+                      display: "inline-block",
+                      textAlign: "right",
+                      fontSize: 10,
+                      padding: 5
+                    }}
+                  >
+                    {item.user_name}
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      height: "100%",
+                      flexDirection: "column",
+                      justifyContent: "space-between",
+                      padding: 5
+                    }}
+                  >
+                    <div
+                      style={{
+                        display: "flex",
+                        fontSize: 12,
+                        height: "100%",
+                        paddingTop: 10
+                      }}
+                    >
+                      {item.title}
+                    </div>
+                    <div>
+                      <div style={{ textAlign: "left", fontSize: 12 }}>
+                        판매량 : {item.number_of_sales}
+                      </div>
+                      <ReactStars
+                        count={5}
+                        value={item.avg_star}
+                        edit={false}
+                      />
+                    </div>
+                  </div>
                 </div>
-                <div>{item.user_name}</div>
-                <div>{item.title}</div>
-                <div>판매량 : {item.number_of_sales}</div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       );
     } else {
