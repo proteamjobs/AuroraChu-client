df93dd7d9dd2cecdb27ec87c4f102e3984a212ec
sehyeonpark <ongsim1@naver.com>
2019-08-28 19:20:52 +0900

    mypage 작업중 1차 푸쉬

--- src/pages/Mypage/FixPassword.js
+++ src/pages/Mypage/FixPassword.js
@@ -1,13 +1,113 @@
 import React, { Component } from "react";
+import {
+  Layout,
+  Menu,
+  Spin,
+  Progress,
+  Avatar,
+  Button,
+  Radio,
+  Icon,
+  Breadcrumb,
+  Modal,
+  Form,
+  Input
+} from "antd";
 
 class FixPassword extends Component {
+  state = {
+    success: null
+  };
+
+  handleSubmit = async e => {
+    const token = await sessionStorage.getItem("token");
+
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+
+        let data = {
+          oldPassword: values.password,
+          newPassword: values.newPassword
+        };
+        fetch("http://13.209.78.148:8080/users/password", {
+          method: "PUT",
+          headers: {
+            Authorization: `JWT ${token}`,
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(data)
+        })
+          .then(res => res.json())
+          .then(json => {
+            console.log(json);
+            if (json.success) {
+              alert("비밀번호 변경 완료!");
+            } else {
+              alert("실패하였습니다. 다시 시도해주세요!");
+            }
+          });
+      }
+    });
+  };
+
   render() {
+    const { getFieldDecorator } = this.props.form;
     return (
-      <div>
-        <div>비밀번호 변경</div>
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          height: "100%",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <Form
+          // onSubmit={() => {
+          //   this.handleSubmit();
+          // }}
+          style={{ width: "80%" }}
+          layout={"vertical"}
+        >
+          <Form.Item label="이전 비밀번호">
+            {getFieldDecorator("password", {
+              rules: [
+                { required: true, message: "기존 비밀번호를 입력해주세요!" }
+              ]
+            })(<Input.Password />)}
+          </Form.Item>
+          <Form.Item label="새로운 비밀번호">
+            {getFieldDecorator("newPassword", {
+              rules: [
+                { required: true, message: "새로운 비밀번호를 입력해주세요!" }
+              ]
+            })(<Input.Password />)}
+          </Form.Item>
+          <Form.Item label="새로운 비밀번호 확인">
+            {getFieldDecorator("newPasswordReplay", {
+              rules: [
+                { required: true, message: "새로운 비밀번호를 입력해주세요!" }
+              ]
+            })(<Input.Password />)}
+          </Form.Item>
+          <Form.Item>
+            <Button
+              onClick={() => {
+                this.handleSubmit();
+              }}
+              type="primary"
+              htmlType="submit"
+            >
+              변경하기
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
     );
   }
 }
 
-export default FixPassword;
+const WrappedApp = Form.create({ name: "coordinated" })(FixPassword);
+
+export default WrappedApp;
--- src/pages/Mypage/index.js
+++ src/pages/Mypage/index.js
@@ -9,7 +9,10 @@ import {
   Button,
   Radio,
   Icon,
-  Breadcrumb
+  Breadcrumb,
+  Modal,
+  Upload,
+  Input
 } from "antd";
 import "./Mypage.css";
 import ReactStars from "react-stars";
@@ -27,13 +30,22 @@ import Store from "../../mobx/signinStore";
 import { observer } from "mobx-react";
 const { Content, Sider, Header, Footer } = Layout;
 const { SubMenu } = Menu;
+const { Search } = Input;
 
 @observer
 class Mypage extends Component {
   state = {
     menuKey: "1",
     menuTitle: "거래 내역 조회",
-    userInfo: {}
+    userInfo: {},
+    profileModalVisiable: false,
+    nicknameModalVisiable: false,
+    fileList: [],
+    file: null,
+    uploading: false,
+    loading: false,
+    nicknameCheck: null,
+    changeNickname: null
   };
 
   async getUserData() {
@@ -59,11 +71,128 @@ class Mypage extends Component {
     }
   }
 
+  showModal(modal) {
+    if (modal === 1) {
+      this.setState({
+        profileModalVisiable: true
+      });
+    } else {
+      this.setState({
+        nicknameModalVisiable: true
+      });
+    }
+  }
+
+  handleUpload = async () => {
+    const token = await sessionStorage.getItem("token");
+    const { fileList } = this.state;
+    const { file } = this.state;
+    const formData = new FormData();
+
+    formData.append("imageFile", fileList[0]);
+    // formData.append("imageFile", file);
+
+    this.setState({
+      uploading: true
+    });
+
+    fetch("http://13.209.78.148:8080/users/profile_img", {
+      method: "PUT",
+      headers: {
+        Authorization: `JWT ${token}`
+      },
+      body: formData
+    })
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+        if (json.success) {
+          this.setState({
+            fileList: [],
+            uploading: false,
+            profileModalVisiable: false
+          });
+          this.getUserData();
+        }
+      });
+  };
+
+  nicknameCheck(nickname) {
+    fetch(`http://13.209.78.148:8080/users/verify?nickname=${nickname}`)
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+        if (json.success) {
+          this.setState({
+            nicknameCheck: true,
+            changeNickname: nickname
+          });
+        } else {
+          this.setState({
+            nicknameCheck: false
+          });
+        }
+      });
+  }
+
+  async changeNickname(nickname) {
+    const token = await sessionStorage.getItem("token");
+    let data = {
+      newNickName: nickname
+    };
+    console.log(data);
+
+    fetch("http://13.209.78.148:8080/users/nickname", {
+      method: "PUT",
+      headers: {
+        Authorization: `JWT ${token}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+        if (json.success) {
+          this.setState({
+            nicknameCheck: null,
+            changeNickname: null,
+            nicknameModalVisiable: false
+          });
+        }
+        this.getUserData();
+      });
+  }
+
   componentDidMount() {
     this.getUserData();
   }
 
   render() {
+    const { uploading, fileList, file, imageUrl } = this.state;
+    const props = {
+      onRemove: file => {
+        this.setState(state => {
+          const index = state.fileList.indexOf(file);
+          const newFileList = state.fileList.slice();
+          newFileList.splice(index, 1);
+          return {
+            fileList: newFileList
+          };
+        });
+      },
+      beforeUpload: file => {
+        this.setState(state => ({
+          fileList: [...state.fileList, file]
+        }));
+        // this.setState({
+        //   file: file
+        // });
+        return false;
+      },
+      fileList
+    };
+
     if (this.state.userInfo !== {}) {
       return (
         <Layout className="layout">
@@ -85,12 +214,114 @@ class Mypage extends Component {
                 className="sider-wrapper"
               >
                 <div className="user-wrapper">
-                  <img
-                    src={this.state.userInfo.profile_url}
-                    style={{ width: 80, height: 80 }}
-                  />
-                  <div className="marinBottom">
-                    {this.state.userInfo.nickname}
+                  <Avatar src={this.state.userInfo.profile_url} size={80} />
+                  <Button
+                    style={{ marginTop: 10 }}
+                    size="small"
+                    onClick={() => {
+                      this.showModal(1);
+                    }}
+                  >
+                    편집
+                  </Button>
+                  <Modal
+                    title="프로필 변경"
+                    visible={this.state.profileModalVisiable}
+                    footer={null}
+                    onCancel={() => {
+                      this.setState({
+                        profileModalVisiable: false,
+                        fileList: [],
+                        file: null,
+                        uploading: false,
+                        loading: false
+                      });
+                    }}
+                  >
+                    <Upload {...props}>
+                      <Button>
+                        <Icon type="upload" /> Select File
+                      </Button>
+                    </Upload>
+                    <Button
+                      type="primary"
+                      onClick={this.handleUpload}
+                      disabled={fileList.length === 0}
+                      loading={uploading}
+                      style={{ marginTop: 16 }}
+                    >
+                      변경하기
+                    </Button>
+                  </Modal>
+                  <div className="marginBottom">
+                    <span style={{ marginRight: 5 }}>
+                      {this.state.userInfo.nickname}
+                    </span>
+                    <Button
+                      size="small"
+                      onClick={() => {
+                        this.showModal(2);
+                      }}
+                    >
+                      편집
+                    </Button>
+                    <Modal
+                      title="닉네임 변경"
+                      visible={this.state.nicknameModalVisiable}
+                      footer={null}
+                      onCancel={() => {
+                        this.setState({
+                          nicknameModalVisiable: false,
+                          nicknameCheck: null,
+                          changeNickname: null
+                        });
+                      }}
+                    >
+                      <Search
+                        placeholder="input search text"
+                        enterButton="중복검사"
+                        size="large"
+                        onChange={() => {
+                          this.setState({
+                            nicknameCheck: null,
+                            changeNickname: null
+                          });
+                        }}
+                        onSearch={value => {
+                          let pattern = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|\*]+$/;
+                          let valueCheck = pattern.exec(value);
+                          if (valueCheck !== null) {
+                            this.nicknameCheck(value);
+                          } else {
+                            alert(
+                              "닉네임은 한글 / 숫자 / 영문 만 사용하실 수 있습니다."
+                            );
+                          }
+                        }}
+                      />
+                      {this.state.nicknameCheck === true ? (
+                        <p style={{ color: "green" }}>
+                          사용 가능한 닉네임 입니다.
+                        </p>
+                      ) : this.state.nicknameCheck === false ? (
+                        <p style={{ color: "red" }}>
+                          이미 사용중인 닉네임 입니다.
+                        </p>
+                      ) : null}
+                      <p style={{ color: "red" }}>
+                        닉네임은 한글 / 숫자 / 영문 만 사용하실 수 있습니다.
+                      </p>
+                      <Button
+                        type="primary"
+                        onClick={() => {
+                          this.changeNickname(this.state.changeNickname);
+                        }}
+                        disabled={this.state.changeNickname === null}
+                        style={{ marginTop: 16 }}
+                      >
+                        변경하기
+                      </Button>
+                    </Modal>
                   </div>
                   <ReactStars count={5} value={5} edit={false} />
                 </div>
@@ -172,7 +403,7 @@ class Mypage extends Component {
                   </SubMenu>
                 </Menu>
               </Sider>
-              <Content style={{ padding: "0 24px", minHeight: 280 }}>
+              <Content className="mypage-content">
                 {this.state.menuKey === "1" ? (
                   <OrderList />
                 ) : this.state.menuKey === "2" ? (
