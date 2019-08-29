2f52a237337ad16c6c89ed486d72016a8aaa1e74
sehyeonpark <ongsim1@naver.com>
2019-08-29 18:34:54 +0900

    거래내역조회 default complete

--- src/pages/Mypage/OrderList.js
+++ src/pages/Mypage/OrderList.js
@@ -1,10 +1,61 @@
 import React, { Component } from "react";
+import { Tabs, Table } from "antd";
+
+const { TabPane } = Tabs;
+
+const columns = [
+  {
+    title: "상품명",
+    dataIndex: "name",
+    render: text => <a>{text}</a>
+  },
+  {
+    title: "마케터",
+    className: "marketer",
+    dataIndex: "marketer"
+  },
+  {
+    title: "구매 일시",
+    dataIndex: "date"
+  },
+  {
+    title: "",
+    dataIndex: "button"
+  }
+];
 
 class OrderList extends Component {
+  callback(key) {
+    console.log(key);
+  }
+
   render() {
     return (
-      <div>
-        <div>거래 내역 조회</div>
+      <div
+        style={{ width: "100%", height: "100%", padding: 30, paddingTop: 10 }}
+      >
+        <div>구매 정보</div>
+        <div style={{ marginBottom: 10 }}>
+          <Tabs defaultActiveKey="1" onChange={this.callback}>
+            <TabPane tab="구매 진행중" key="1">
+              <Table columns={columns} bordered />
+            </TabPane>
+            <TabPane tab="구매 완료" key="2">
+              <Table columns={columns} bordered />
+            </TabPane>
+          </Tabs>
+        </div>
+        <div>판매 정보</div>
+        <div>
+          <Tabs defaultActiveKey="1" onChange={this.callback}>
+            <TabPane tab="판매 진행중" key="1">
+              <Table columns={columns} bordered />
+            </TabPane>
+            <TabPane tab="판매 완료" key="2">
+              <Table columns={columns} bordered />
+            </TabPane>
+          </Tabs>
+        </div>
       </div>
     );
   }
