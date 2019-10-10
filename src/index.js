4d83eb1956c66f79c5cc0a260f4fb095f361f3a0
yohan yoo <public.afant@gmail.com>
2019-10-10 14:51:41 +0900

    no message

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import "babel-polyfill";
+import "react-app-polyfill/ie9"; // For IE 9-11 support
+// import "react-app-polyfill/ie11"; // For IE 11 support
+import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
